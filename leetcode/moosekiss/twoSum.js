983d5af904e6d24d7da35ec74117d4636933564c
moosekiss <gusdn6526@gmail.com>
2022-02-05 00:08:47 +0900

    Solve first problem

--- leetcode/moosekiss/twoSum.js
+++ leetcode/moosekiss/twoSum.js
@@ -1,1 +1,19 @@
-console.log("git init");
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ let twoSum = function(nums, target) {
+
+    for(let i=0; i<=nums.length; i++) {
+        for(let j=i+1; j<=nums.length-1; j++) {
+            if(nums[i]+nums[j] == target) {
+                return [i, j];
+            }
+        }
+    }
+
+};
+
+let result = twoSum([3,3], 6);
+console.log(result);
